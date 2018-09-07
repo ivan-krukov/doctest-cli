5ebd900112560984e2999bd7c08b4d99e87abe16
Ivan Krukov <ivan.o.kryukov@gmail.com>
2018-09-07 11:43:41 -0600

    added stderr handling

---
+++ test/example.sh
@@ -0,0 +1,37 @@
+# This is a comment
+
+# Test success
+>>> echo hello
+hello
+
+# Test fail
+>>> echo hello
+bye
+
+# Test return status
+>>> false
+$?=1
+
+# Test no output
+>>> true
+
+# Test multiline
+>>> echo "hello\nworld"
+hello
+world
+
+# Test piping
+>>> echo 'hello' | wc -c
+6
+
+# Test file piping
+>>> echo 'hello' > tmp.txt && wc -l tmp.txt && rm tmp.txt
+1 tmp.txt
+
+# Test fail unknown
+>>> take 3
+3
+
+# Test stderr
+>>> echo hello > /dev/stderr
+! hello
