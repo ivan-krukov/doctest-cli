025b923723db9b320da035bcc011c09c5dd0c81e
Ivan Krukov <ivan.o.kryukov@gmail.com>
2018-09-07 16:54:56 -0600

    remove old python, verbose test

--- test/meta.sh
+++ test/meta.sh
@@ -1,16 +1,71 @@
 # Test the test
->>> bin/doctest-cli test/example.sh
+>>> bin/doctest-cli --verbose test/example.sh
 
 doctest-cli: test/example.sh
+------------------------------------------------------------
+Test passed:
+  echo hello
+With STDERR:
+ 
+With STDOUT:
+ hello
+With RETURN:
+ 0
 ------------------------------------------------------------
 Error while running:
   echo hello
 With STDERR:
-
+  
 Expected STDOUT:
   bye
 Got:
   hello
+------------------------------------------------------------
+Test passed:
+  false
+With STDERR:
+ 
+With STDOUT:
+ 
+With RETURN:
+ 1
+------------------------------------------------------------
+Test passed:
+  true
+With STDERR:
+ 
+With STDOUT:
+ 
+With RETURN:
+ 0
+------------------------------------------------------------
+Test passed:
+  printf "hello\nworld"
+With STDERR:
+ 
+With STDOUT:
+ hello
+ world
+With RETURN:
+ 0
+------------------------------------------------------------
+Test passed:
+  echo 'hello' | wc -c
+With STDERR:
+ 
+With STDOUT:
+ 6
+With RETURN:
+ 0
+------------------------------------------------------------
+Test passed:
+  echo 'hello' > tmp.txt && wc -l tmp.txt && rm tmp.txt
+With STDERR:
+ 
+With STDOUT:
+ 1 tmp.txt
+With RETURN:
+ 0
 ------------------------------------------------------------
 Error while running:
   take 3
@@ -19,7 +74,15 @@ With STDERR:
 Expected RETURN: 0
 Got: 127
 ------------------------------------------------------------
+Test passed:
+  echo hello > /dev/stderr
+With STDERR:
+ hello
+With STDOUT:
+ 
+With RETURN:
+ 0
+------------------------------------------------------------
 Tests ran: 9
 Tests passed: 7
 Tests failed: 2
-$?=0
